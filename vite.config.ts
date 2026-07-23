2c85ea529729e51cec22bb4e42ce38619bc96ecc
startupskala-source <startupskala@gmail.com>
2026-07-23 11:01:02 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     tanstackStart({ server: { entry: "server" } }),
+    nitro(),
     viteReact(),
     tailwindcss(),
     tsConfigPaths(),
